9b90cec92284c7d4c17a0752278aa1c555831ee5
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-03 20:50:35 +0800

    Update main.c

--- app_pojav_zh/src/main/jni/tinywrapper/main.c
+++ app_pojav_zh/src/main/jni/tinywrapper/main.c
@@ -1,9 +1,11 @@
 //#import <Foundation/Foundation.h>
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "GL/gl.h"
-#include "GLES3/gl32.h"
+#include <GLES3/gl32.h>
 #include "string_utils.h"
 
 #define LOOKUP_FUNC(func) \
