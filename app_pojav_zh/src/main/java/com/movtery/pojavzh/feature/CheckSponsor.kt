f3acd1985675e0626d936b5138dfe94a2d927ec4
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-03 20:34:07 +0800

    Update CheckSponsor.kt

--- app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/CheckSponsor.kt
+++ app_pojav_zh/src/main/java/com/movtery/pojavzh/feature/CheckSponsor.kt
@@ -53,8 +53,8 @@ object CheckSponsor {
                     throw IOException("Unexpected code $response")
                 } else {
                     try {
-                        Objects.requireNonNull(response.body())
-                        val responseBody = response.body()!!.string()
+                        Objects.requireNonNull(response.body)
+                        val responseBody = response.body!!.string()
 
                         val originJson = JSONObject(responseBody)
                         val rawBase64 = originJson.getString("content")
