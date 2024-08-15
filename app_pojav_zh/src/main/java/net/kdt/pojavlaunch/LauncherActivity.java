8022fcc2fc685d563eec51aede0f6bf2a5bd7b03
Vera-Firefly <1964762970@qq.com>
2024-08-15 11:31:32 +0800

    同步(3ecaab2)

--- app_pojav_zh/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
+++ app_pojav_zh/src/main/java/net/kdt/pojavlaunch/LauncherActivity.java
@@ -307,6 +307,7 @@ public class LauncherActivity extends BaseActivity {
                 }
         );
         bindViews();
+        setPageOpacity();
         ZHTools.setBackgroundImage(this, BackgroundType.MAIN_MENU, mBackgroundView);
 
         checkNotificationPermission();
@@ -357,6 +358,7 @@ public class LauncherActivity extends BaseActivity {
         super.onResume();
         ContextExecutor.setActivity(this);
         mInstallTracker.attach();
+        setPageOpacity();
     }
 
     @Override
@@ -532,6 +534,10 @@ public class LauncherActivity extends BaseActivity {
                     })
                     .buildDialog();
         }
+    }
+
+    private void setPageOpacity() {
+        if (mFragmentView != null) mFragmentView.setAlpha((float) LauncherPreferences.PREF_PAGE_OPACITY / 100);
     }
 
     /** Stuff all the view boilerplate here */
