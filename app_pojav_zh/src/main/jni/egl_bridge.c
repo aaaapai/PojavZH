a3412ed9ac512e7b50299aa70f43d1b477835c4f
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-16 19:15:55 +0800

    Update egl_bridge.c

--- app_pojav_zh/src/main/jni/egl_bridge.c
+++ app_pojav_zh/src/main/jni/egl_bridge.c
@@ -198,19 +198,6 @@ int pojavInitOpenGL() {
     if (strncmp("opengles2", renderer, 9) == 0) {
         pojav_environ->config_renderer = RENDERER_GL4ES;
         set_gl_bridge_tbl();
-        static void vrend_resource_buffer_copy(UNUSED struct vrend_context *ctx,
-                                         struct vrend_resource *src_res,
-                                         struct vrend_resource *dst_res,
-                                         uint32_t dstx, uint32_t srcx,
-                                         uint32_t width)
-        {
-          glBindBuffer(GL_COPY_READ_BUFFER, src_res->gl_id);
-          glBindBuffer(GL_COPY_WRITE_BUFFER, dst_res->gl_id);
-
-          glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, srcx, dstx, width);
-          glBindBuffer(GL_COPY_READ_BUFFER, 0);
-          glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
-        };
     } else if (strcmp(renderer, "vulkan_zink") == 0) {
         pojav_environ->config_renderer = RENDERER_VK_ZINK;
         load_vulkan();
