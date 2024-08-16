991000eeb80655d93e342ad63d9833a8bb1b7fac
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-08-16 15:35:45 +0800

    Update egl_bridge.c

--- app_pojav_zh/src/main/jni/egl_bridge.c
+++ app_pojav_zh/src/main/jni/egl_bridge.c
@@ -38,9 +38,21 @@
 // This means that you are forced to have this function/variable for ABI compatibility
 #define ABI_COMPAT __attribute__((unused))
 
-void bindBuffer();
-void gl4es_glCopyBufferSubData();
+if (strncmp("opengles2", renderer, 9) == 0) {
+   static void vrend_resource_buffer_copy(UNUSED struct vrend_context *ctx,
+                                       struct vrend_resource *src_res,
+                                       struct vrend_resource *dst_res,
+                                       uint32_t dstx, uint32_t srcx,
+                                       uint32_t width)
+   {
+      glBindBuffer(GL_COPY_READ_BUFFER, src_res->gl_id);
+      glBindBuffer(GL_COPY_WRITE_BUFFER, dst_res->gl_id);
 
+      glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, srcx, dstx, width);
+      glBindBuffer(GL_COPY_READ_BUFFER, 0);
+      glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
+   }
+}
 struct PotatoBridge {
 
     /* EGLContext */ void* eglContext;
@@ -201,10 +213,8 @@ int pojavInitOpenGL() {
     if (strncmp("opengles2", renderer, 9) == 0) {
         pojav_environ->config_renderer = RENDERER_GL4ES;
         set_gl_bridge_tbl();
-        #define GL_ARRAY_BUFFER_BINDING bindBuffer
-        #define GL_MAP_FLUSH_EXPLICIT_BIT GL_MAP_FLUSH_EXPLICIT_BIT_EXT 
-        #define GL_COPY_READ_BUFFER gl4es_glCopyBufferSubData
-        #define GL_COPY_WRITE_BUFFER gl4es_glCopyBufferSubData
+        #define GL_ARRAY_BUFFER_BINDING GL_ARRAY_BUFFER
+        #define GL_MAP_FLUSH_EXPLICIT_BIT GL_MAP_FLUSH_EXPLICIT_BIT_EXT
     } else if (strcmp(renderer, "vulkan_zink") == 0) {
         pojav_environ->config_renderer = RENDERER_VK_ZINK;
         load_vulkan();
