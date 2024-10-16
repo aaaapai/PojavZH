13700efc90f49be52b14d32eb9fff889a175b474
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-10-16 23:50:23 +0800

    Delete app_pojav_zh/src/main/jni/driver_helper/hook.h

--- app_pojav_zh/src/main/jni/driver_helper/hook.h
+++
@@ -1,7 +0,0 @@
-//NOLINTBEGIN
-static void* (*android_dlopen_ext_p)(const char* filename,
-                                  int flags,
-                                  const android_dlextinfo* extinfo,
-                                  const void* caller_addr);
-static struct android_namespace_t* (*android_get_exported_namespace_p)(const char* name);
-//NOLINTEND
