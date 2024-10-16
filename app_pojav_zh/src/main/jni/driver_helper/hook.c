0a372aa7c5e8bf543b44c7ef60679c1a4a6fe847
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-10-16 23:50:52 +0800

    Update hook.c

--- app_pojav_zh/src/main/jni/driver_helper/hook.c
+++ app_pojav_zh/src/main/jni/driver_helper/hook.c
@@ -5,7 +5,13 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "hook.h"
+//NOLINTBEGIN
+static void* (*android_dlopen_ext_p)(const char* filename,
+                                  int flags,
+                                  const android_dlextinfo* extinfo,
+                                  const void* caller_addr);
+static struct android_namespace_t* (*android_get_exported_namespace_p)(const char* name);
+//NOLINTEND
 // Silence the warnings about using reserved identifiers (we need to link to these to not pollute the global symtab)
 static void* ready_handle;
 
