65bb534e97c59835457e02ed8dd9e2d6e1265213
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-10-21 23:26:50 +0800

    Update Android.mk

--- app_pojav_zh/src/main/jni/Android.mk
+++ app_pojav_zh/src/main/jni/Android.mk
@@ -21,11 +21,6 @@ LOCAL_SRC_FILES := angle/libGLESv2_angle.so
 include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := EGL_angle
-LOCAL_SRC_FILES := angle/libEGL_angle.so
-include $(PREBUILT_SHARED_LIBRARY)
-include $(CLEAR_VARS)
-
 LOCAL_MODULE := ltw_angle
 LOCAL_SHARED_LIBRARIES := GLESv2_angle ltw
 include $(BUILD_SHARED_LIBRARY)
@@ -56,8 +51,8 @@ LOCAL_SRC_FILES := \
     driver_helper/nsbypass.c
 
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-LOCAL_CFLAGS += -DADRENO_POSSIBLE
-LOCAL_CFLAGS += -O3 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -std=c23
+// LOCAL_CFLAGS += -DADRENO_POSSIBLE
+LOCAL_CFLAGS += -O3 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -pedantic -std=c23
 LOCAL_LDLAGS += -flto=thin
 LOCAL_LDLIBS += -lEGL -lGLESv3
 endif
@@ -68,7 +63,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := linkerhook
 LOCAL_SRC_FILES := driver_helper/hook.c
 LOCAL_LDFLAGS := -z global
-LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -std=c23
+LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -pedantic -std=c23
 LOCAL_LDLAGS += -flto=thin
 include $(BUILD_SHARED_LIBRARY)
 #endif
@@ -81,7 +76,7 @@ LOCAL_MODULE := istdio
 LOCAL_SHARED_LIBRARIES := bytehook
 LOCAL_SRC_FILES := \
     stdio_is.c
-LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -std=c23
+LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wall -pedantic -std=c23
 LOCAL_LDLAGS += -flto=thin
 include $(BUILD_SHARED_LIBRARY)
 
@@ -89,7 +84,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := pojavexec_awt
 LOCAL_SRC_FILES := \
     awt_bridge.c
-LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wno-int-conversion -Wall -std=c23
+LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -Wno-int-conversion -Wall -pedantic -std=c23
 LOCAL_LDLAGS += -flto=thin
 include $(BUILD_SHARED_LIBRARY)
 
@@ -112,6 +107,6 @@ LOCAL_MODULE := awt_xawt
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES := awt_headless
 LOCAL_SRC_FILES := xawt_fake.c
-LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread
+LOCAL_CFLAGS += -O2 -fPIC -DPIC -flto=thin -fwhole-program-vtables -mllvm -polly -pthread -pedantic
 LOCAL_LDLAGS += -flto=thin
 include $(BUILD_SHARED_LIBRARY)
