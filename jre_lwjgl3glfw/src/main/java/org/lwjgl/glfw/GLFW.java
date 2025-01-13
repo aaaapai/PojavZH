a23d0c667546efeed4f627255af90a5a8a3b358d
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-13 12:44:54 +0800

    Update GLFW.java

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
@@ -925,9 +925,8 @@ public class GLFW
         return GLFW_NO_ERROR;
     }
 
-    @Nullable
     @NativeType("GLFWvidmode const *")
-    public static GLFWVidMode.Buffer glfwGetVideoModes(@NativeType("GLFWmonitor *") long monitor) {
+    public static GLFWVidMode.@Nullable Buffer glfwGetVideoModes(@NativeType("GLFWmonitor *") long monitor) {
         MemoryStack stack = stackGet(); int stackPointer = stack.getPointer();
         try {
             // long __result = nglfwGetVideoModes(monitor, memAddress(count));
@@ -1084,7 +1083,7 @@ public class GLFW
         internalGetWindow(window).title = title;
     }
 
-    public static void glfwSetWindowIcon(@NativeType("GLFWwindow *") long window, @Nullable @NativeType("GLFWimage const *") GLFWImage.Buffer images) {}
+    public static void glfwSetWindowIcon(@NativeType("GLFWwindow *") long window, @NativeType("GLFWimage const *") GLFWImage.@Nullable Buffer images) {}
 
     public static void glfwPollEvents() {
         if (!mGLFWIsInputReady) {
