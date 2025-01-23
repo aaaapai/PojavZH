add976d8f1cad79f9ad76ac0f909a1ea3d8612fd
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-01-23 18:35:13 +0800

    Update gl_bridge.c

--- ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
+++ ZalithLauncher/src/main/jni/ctxbridges/gl_bridge.c
@@ -48,8 +48,8 @@ gl_render_window_t* gl_init_context(gl_render_window_t *share) {
     gl_render_window_t* bundle = malloc(sizeof(gl_render_window_t));
     memset(bundle, 0, sizeof(gl_render_window_t));
     int ZL_GLES_BIT = strtol(getenv("ZL_GLES_BIT"), NULL, 0);
-    if (ZL_GLES_BIT < 0 || ZL_GLES_BIT > INT16_MAX) ZL_GLES_BIT = EGL_OPENGL_ES2_BIT;
-    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_ALPHA_MASK_SIZE, 8, EGL_SURFACE_TYPE, EGL_WINDOW_BIT|EGL_PBUFFER_BIT, EGL_CONFORMANT, EGL_OPENGL_ES2_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE };
+    if (ZL_GLES_BIT < 0 || ZL_GLES_BIT > INT16_MAX) ZL_GLES_BIT = EGL_OPENGL_ES3_BIT_KHR;
+    EGLint egl_attributes[] = { EGL_BLUE_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 24, EGL_ALPHA_MASK_SIZE, 8, EGL_SURFACE_TYPE, EGL_WINDOW_BIT|EGL_PBUFFER_BIT, EGL_CONFORMANT, ZL_GLES_BIT, EGL_RENDERABLE_TYPE, ZL_GLES_BIT, EGL_NONE };
     EGLint num_configs = 0;
 
     if (eglChooseConfig_p(g_EglDisplay, egl_attributes, NULL, 0, &num_configs) != EGL_TRUE)
